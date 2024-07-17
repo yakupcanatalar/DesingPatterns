eca353e17fb30d1a4ff8bf41138dbbb303931690
Yakup Can Atalar <yakupcan.atalar@siemens.com>
2024-07-17 13:02:09 +0300

    NullObjectReferance design pattern implemented

---
+++ NullObjectPattern/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObjectPattern
+{
+    public class Dog : IAnimal
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Hav-Hav");
+        }
+    }
+}
---
+++ NullObjectPattern/Program.cs
@@ -0,0 +1,31 @@
+﻿using NullObjectPattern;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal dog = GetAnimal("Dog");
+        dog.MakeSound();
+
+        //Olmayan bi animal
+        IAnimal cat = GetAnimal("Cat");
+        cat.MakeSound();
+
+        // Null kontrol olmadan null referans yerine Null Object kullanımı
+        IAnimal animal = GetAnimal("Unknown");
+        animal.MakeSound();  // Hiçbir şey yapmaz
+
+    }
+
+    public static IAnimal GetAnimal(string type)
+    {
+        if (type == "Dog")
+        {
+            return new Dog();
+        }
+        else
+        {
+            return new NullAnimal(); // null yerine NullObjectAnimal döndürüyoruz
+        }
+    }
+}
