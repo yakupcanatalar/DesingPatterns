076268e7a51d128c2619296ddd43e7e9291a0370
Yakup Can Atalar <yakupcan.atalar@siemens.com>
2024-07-17 11:53:58 +0300

    Bridge pattern implemented

---
+++ BridgePattern/MovieLicense.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgePattern
+{
+    public class MovieLicense
+    {
+        private readonly Discount _discount;
+        private readonly LicenseType _licenseType;
+        private readonly SpecialOffer _specialOffer;
+        public string Movie { get; }
+        public DateTime PurchaseTime { get; }
+
+        public MovieLicense(string movie, DateTime purchaseTime, Discount discount,LicenseType licenseType, SpecialOffer specialOffer=SpecialOffer.None)
+        {
+            Movie = movie;
+            PurchaseTime = purchaseTime;
+            _discount = discount;
+            _licenseType = licenseType;
+            _specialOffer = specialOffer;
+        }
+
+        public decimal GetPrice()
+        {
+            int discount = GetDiscount();
+            decimal multiplier = 1 - discount / 100m;
+            return GetBasePrice() * multiplier;
+        }
+
+        private int GetDiscount()
+        {
+            return _discount switch
+            {
+                Discount.None => 0,
+                Discount.Military => 10,
+                Discount.Senior => 20,
+
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private decimal GetBasePrice()
+        {
+            return _licenseType switch
+            {
+                LicenseType.TwoDays => 4,
+                LicenseType.LifeLong => 8,
+
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        public DateTime? GetExpirationDate()
+        {
+            DateTime? expirationDate = GetBaseExpirationDate();
+            TimeSpan extension = GetSpecialOfferExtension();
+
+            return expirationDate?.Add(extension);
+        }
+
+        private DateTime? GetBaseExpirationDate()
+        {
+            return _licenseType switch
+            {
+                LicenseType.TwoDays => PurchaseTime.AddDays(2)  as DateTime?,
+                LicenseType.LifeLong => null,
+
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+
+        private TimeSpan GetSpecialOfferExtension()
+        {
+            return _specialOffer switch
+            {
+                SpecialOffer.None => TimeSpan.Zero,
+                SpecialOffer.TwoDaysExtension => TimeSpan.FromDays(2),
+
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+    }
+
+    public enum Discount
+    {
+        None,
+        Military,
+        Senior
+    }
+
+    public enum LicenseType
+    {
+        TwoDays,
+        LifeLong
+    }
+
+    public enum SpecialOffer
+    {
+        None,
+        TwoDaysExtension
+    }
+}
---
+++ BridgePattern/Program.cs
@@ -0,0 +1,53 @@
+﻿using BridgePattern;
+
+public class Program
+{
+    public static void Main()
+    {
+        DateTime now = DateTime.Now;
+
+        var license1 = new MovieLicense("Secret Life of Pets", now, Discount.None, LicenseType.TwoDays);
+        var license2 = new MovieLicense("Matrix", now, Discount.None,LicenseType.LifeLong);
+
+        PrintLicenseDetails(license1);
+        PrintLicenseDetails(license2);
+
+        var license3 = new MovieLicense("Secret Life of Pets", now, Discount.Military, LicenseType.LifeLong);
+        var license4 = new MovieLicense("Matrix", now, Discount.Senior, LicenseType.TwoDays);
+
+        PrintLicenseDetails(license3);
+        PrintLicenseDetails(license4);
+
+        var license5 = new MovieLicense("Matrix", now, Discount.Senior, LicenseType.TwoDays, SpecialOffer.TwoDaysExtension);
+
+        PrintLicenseDetails(license5);
+
+        Console.ReadKey();
+    }
+
+    private static void PrintLicenseDetails(MovieLicense license)
+    {
+        Console.WriteLine($"Movie: {license.Movie}");
+        Console.WriteLine($"Price: {GetPrice(license)}");
+        Console.WriteLine($"Valid for: {GetValidFor(license)}");
+
+        Console.WriteLine();
+    }
+
+    private static string GetPrice(MovieLicense license)
+    {
+        return $"${license.GetPrice():0.00}";
+    }
+
+    private static string GetValidFor(MovieLicense license)
+    {
+        DateTime? expirationDate = license.GetExpirationDate();
+
+        if (expirationDate == null)
+            return "Forever";
+
+        TimeSpan timeSpan = expirationDate.Value - DateTime.Now;
+
+        return $"{timeSpan.Days}d {timeSpan.Hours}h {timeSpan.Minutes}m";
+    }
+}
